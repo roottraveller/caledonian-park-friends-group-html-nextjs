20ff91bc81d43dfc62197bd41bb9f2ca15f2e5ec
rimaurya <rimaurya@expediagroup.com>
2023-09-01 00:28:36 +0100

    gh pages build fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   // reactStrictMode: true,
   // basePath: '/github-pages',
+  exportPathMap: async function() {
+    return {
+      '/': { page: '/' }, // Map the root page to '/'
+      // Add other custom route mappings here if needed
+    };
+  },
   experimental: {
     appDir: true,
   },
