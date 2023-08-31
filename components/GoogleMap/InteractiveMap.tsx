10bc56d090d2767cef60d936a4c3c2a41642841a
rimaurya <rimaurya@expediagroup.com>
2023-08-31 23:57:47 +0100

    build fix

--- components/GoogleMap/InteractiveMap.tsx
+++ components/GoogleMap/InteractiveMap.tsx
@@ -9,18 +9,18 @@ const InteractiveMap = () => {
     };
 
     return (
-        <div style={{ height: '100%', width: '100%' }}>
+        <div style={{height: '100%', width: '100%'}}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY }}
+                bootstrapURLKeys={{key: GOOGLE_MAPS_API_KEY}}
                 defaultCenter={mapCenter}
                 defaultZoom={17}
             >
-                <Marker lat={51.5465575} lng={-0.1239544} text="Caledonian Park" />
+                <Marker lat={51.5465575} lng={-0.1239544} text="Caledonian Park"/>
             </GoogleMapReact>
         </div>
     );
 };
 
-const Marker = ({ text }) => <div className="marker">{text}</div>;
+const Marker = ({lat, lng, text}) => <div className="marker" lat={lat} lng={lng}>{text}</div>;
 
 export default InteractiveMap;
